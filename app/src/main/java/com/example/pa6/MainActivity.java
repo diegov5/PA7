05d231fbd5637540da01fbadd9243eeb48171b56
Diegov5 <diegov0824@hotmail.com>
2019-11-03 13:05:33 -0800

    Set up layouts completely in Java

--- app/src/main/java/com/example/pa6/MainActivity.java
+++ app/src/main/java/com/example/pa6/MainActivity.java
@@ -5,16 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button newNoteButton;
+    GridLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.mainGridLayout);
+        layout.setColumnCount(1);
+        layout.setRowCount(1);
+
+        newNoteButton = new Button(this);
+        newNoteButton.setText("Add New Note");
+
+        GridLayout.Spec rowSpec = GridLayout.spec(0, 1, 1);
+        // row start index, row span, row weight
+        GridLayout.Spec colSpec = GridLayout.spec(0, 1, 1);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        newNoteButton.setLayoutParams(layoutParams);
+        newNoteButton.setOnClickListener(this);
+        layout.addView(newNoteButton);
     }
 
-    public void createNewNote(View view) {
+    @Override
+    public void onClick(View view) {
         Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/pa6/NoteActivity.java
+++ app/src/main/java/com/example/pa6/NoteActivity.java
@@ -3,23 +3,109 @@ package com.example.pa6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.Spinner;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements View.OnClickListener {
+    Spinner categorySpinner;
+    EditText title;
+    EditText content;
+    GridLayout noteGridLayout;
+    Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        noteGridLayout = findViewById(R.id.noteGridLayout);
+        noteGridLayout.setColumnCount(2);
+        noteGridLayout.setRowCount(3);
+
+        createEditText();
+        createSpinner();
+        createContentEditText();
+        createDoneButton();
+    }
+
+    public void createEditText(){
+        title = new EditText(this);
+
+        GridLayout.Spec rowSpec = GridLayout.spec(0, 1);
+        // row start index, row span, row weight
+        GridLayout.Spec colSpec = GridLayout.spec(0,1 , 1);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        title.setLayoutParams(layoutParams);
+        title.setHint("Title");
+
+        noteGridLayout.addView(title);
+    }
+
+    public void createSpinner(){
+        categorySpinner = new Spinner(this);
+
+        GridLayout.Spec rowSpec = GridLayout.spec(0, 1);
+        // row start index, row span, row weight
+        GridLayout.Spec colSpec = GridLayout.spec(1,1 , 1);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        categorySpinner.setLayoutParams(layoutParams);
         String[] arraySpinner = new String[] {
                 "Personal", "School", "Work", "Other"
         };
-        Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
+        categorySpinner.setAdapter(adapter);
+
+        noteGridLayout.addView(categorySpinner);
+    }
+
+    public void createContentEditText(){
+        content = new EditText(this);
+
+        GridLayout.Spec rowSpec = GridLayout.spec(1, 1, 1);
+        // row start index, row span, row weight
+        GridLayout.Spec colSpec = GridLayout.spec(0,2 , 1);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        content.setLayoutParams(layoutParams);
+        content.setHint("Content");
+        content.setGravity(Gravity.CLIP_VERTICAL);
+
+        noteGridLayout.addView(content);
+
+    }
+
+    public void createDoneButton(){
+        doneButton = new Button(this);
+
+        doneButton.setText("Done");
+
+        GridLayout.Spec rowSpec = GridLayout.spec(2, 1);
+        // row start index, row span, row weight
+        GridLayout.Spec colSpec = GridLayout.spec(0, 2, 1);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        doneButton.setLayoutParams(layoutParams);
+        doneButton.setOnClickListener(this);
+        noteGridLayout.addView(doneButton);
+    }
+
+    @Override
+    public void onClick(View view) {
+        this.finish();
     }
 }
